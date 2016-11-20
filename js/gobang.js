88d404e47bb912d4c1f1d230c6474dbd76d4f93e
weieyuan <weieyuan@yeah.net>
2016-11-20 14:00:35 +0800

    格式化代码

--- js/gobang.js
+++ js/gobang.js
@@ -1,4 +1,4 @@
-var Gobang = function(options){
+var Gobang = function(options) {
 	this.options = Object.assign({
 		canvasId: undefined
 	}, options);
@@ -6,43 +6,43 @@ var Gobang = function(options){
 	//0表示下白棋，1表示下黑棋
 	this.wOrB = options.whiteFirst === true ? 0 : 1;
 };
-Gobang.prototype.init = function(){
+Gobang.prototype.init = function() {
 	this.initParams();
-	this.drawBackgroundImg(function(){
-		this.drawBoard();	
+	this.drawBackgroundImg(function() {
+		this.drawBoard();
 	}.bind(this));
 	this.bindEvent();
 }
-Gobang.prototype.initParams = function(){
+Gobang.prototype.initParams = function() {
 	var canvas = document.getElementById(this.options.canvasId);
-    this.canvasWidth = canvas.width;
+	this.canvasWidth = canvas.width;
 	this.canvasHeight = canvas.height;
 	//min表示棋盘的宽高
 	this.min = Math.min(this.canvasWidth, this.canvasHeight);
-    //横竖画15条线，步长表示每个格子的宽度
+	//横竖画15条线，步长表示每个格子的宽度
 	this.step = this.min / 15;
 	this.canvas = canvas;
 	this.context = canvas.getContext("2d");
-	for(var i = 0; i < 15; i++){
+	for (var i = 0; i < 15; i++) {
 		var xGrid = new Array(15);
-		for(var j = 0; j < 15; j++){
+		for (var j = 0; j < 15; j++) {
 			xGrid[j] = 0;
 		}
 		this.grid[i] = xGrid;
 	}
 };
-Gobang.prototype.drawBackgroundImg = function(oAfterLoadImg){
+Gobang.prototype.drawBackgroundImg = function(oAfterLoadImg) {
 	var img = new Image();
 	img.src = "image/background1.png";
-	img.onload = function(){
+	img.onload = function() {
 		this.context.drawImage(img, 0, 0, this.canvasWidth, this.canvasHeight);
 		oAfterLoadImg();
 	}.bind(this);
 };
-Gobang.prototype.drawBoard = function(){
+Gobang.prototype.drawBoard = function() {
 	var padding = this.step / 2;
 	this.context.fillStyle = "#6B6B6B";
-	for(var i = 0; i < 15; i++){
+	for (var i = 0; i < 15; i++) {
 		//横向画15条线
 		this.context.beginPath();
 		this.context.moveTo(padding, padding + i * this.step);
@@ -55,36 +55,35 @@ Gobang.prototype.drawBoard = function(){
 		this.context.stroke();
 	}
 };
-Gobang.prototype.bindEvent = function(){
-	this.canvas.addEventListener("click", function(e){
+Gobang.prototype.bindEvent = function() {
+	this.canvas.addEventListener("click", function(e) {
 		var x = e.offsetX;
 		var y = e.offsetY;
-		if(this.isInBorder(x, y)){
+		if (this.isInBorder(x, y)) {
 			var i = Math.floor(x / this.step);
 			var j = Math.floor(y / this.step);
 			//处理在最大边界上的情况
-			if(i == 15){
+			if (i == 15) {
 				i = 14;
 			}
-			if(j == 15){
+			if (j == 15) {
 				j = 14;
 			}
 			this.drawChessman(i, j);
 		}
 	}.bind(this));
 };
-Gobang.prototype.drawChessman = function(i, j){
+Gobang.prototype.drawChessman = function(i, j) {
 	//判断该点是否已经画过棋子
-	if(this.grid[i][j]){
+	if (this.grid[i][j]) {
 		return;
 	}
 	//设置棋子的颜色,更新内存
-	if(this.wOrB){
+	if (this.wOrB) {
 		this.context.fillStyle = "#000000";
 		this.context.strokeStyle = "#4F4F4F";
 		this.grid[i][j] = 2;
-	}
-	else{
+	} else {
 		this.context.fillStyle = "#FFFFFF";
 		this.context.strokeStyle = "#F8F8F8";
 		this.grid[i][j] = 1;
@@ -101,7 +100,7 @@ Gobang.prototype.drawChessman = function(i, j){
 	//更新wOrB变量
 	this.wOrB = 1 - this.wOrB;
 };
-Gobang.prototype.isInBorder = function(x, y){
+Gobang.prototype.isInBorder = function(x, y) {
 	var backgroundColor = WUtils.getCssPropValue(this.options.canvasId, "backgroundColor");
 	this.context.beginPath();
 	this.context.strokeStyle = backgroundColor;
--- js/utils.js
+++ js/utils.js
@@ -1,10 +1,10 @@
-(function(context){
+(function(context) {
 	var WUtils = context.WUtils = new Object();
 
-	WUtils.getCssPropValue = function(elementId, cssPropValue){
+	WUtils.getCssPropValue = function(elementId, cssPropValue) {
 		var element = document.getElementById(elementId);
 		var style = window.getComputedStyle ? window.getComputedStyle(element, null) : element.style;
 		return style[cssPropValue];
 	};
-	
+
 })(this);
